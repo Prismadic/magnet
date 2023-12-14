0eaf5234dc998bb53b1a0beb88649abecc742bfe
DylanAlloy <alloydylan@icloud.com>
2023-12-14 11:25:18 -0500

    🧲 feat(release): 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='llm_magnet',
-    version='0.0.1',
+    version='0.0.2',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
