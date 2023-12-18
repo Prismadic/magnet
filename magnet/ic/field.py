d8973d3cfccefacb2e0c15954ae8eace851e28ca
DylanAlloy <alloydylan@icloud.com>
2023-12-18 18:19:35 -0500

    🧲 feat(release): it's alive

--- magnet/ic/field.py
+++ magnet/ic/field.py
@@ -4,13 +4,15 @@ from magnet.utils import _f
 class Charge:
     def __init__(self, server):
         self.server = server
-        
+
     async def on(self, frequency: str = 'default'):
         self.frequency = frequency
         try:
             nc = await nats.connect(f'nats://{self.server}:4222')
             self.nc = nc
-            self.sub = await self.nc.subscribe(self.frequency)
+            self.js = self.nc.jetstream()
+            await self.js.add_stream(name=self.frequency, subjects=[self.frequency])
+            self.sub = await self.js.pull_subscribe(self.frequency, 'workers')
             _f("success", f'connected to {self.server}')
         except TimeoutError:
             _f('fatal', f'could not connect to {self.server}')
@@ -21,23 +23,29 @@ class Charge:
         _f('warn', f'disconnected from {self.server}')
     async def pulse(self, packet):
         try:
-            await self.nc.publish(self.frequency, packet)
-        except:
+            await self.js.publish(self.frequency, packet)
+        except Exception as e:
+            print(e)
             _f('fatal', f'could not send data to {self.server}')
+    async def emp(self, name: str = None):
+        await self.js.delete_stream(name=name if name is not None else self.frequency)
+        _f('success', f'{self.frequency} stream deleted')
 
 class Resonator:
+
     def __init__(self, server):
         self.server = server
-
     async def on(self, frequency: str = 'default', cb=print):
         self.frequency = frequency
         self.nc = await nats.connect(f'nats://{self.server}:4222')
-        self.sub = await self.nc.subscribe(self.frequency)
+        self.js = self.nc.jetstream()
+        self.sub =  await self.js.pull_subscribe(self.frequency, stream=self.frequency)
         while True:
-            msg = await self.sub.next_msg(timeout=1000)
-            cb(msg) # customizable callback
+            msgs = await self.sub.fetch(batch=10, timeout=60)
+            for msg in msgs:
+                cb(msg)
     async def off(self):
-        await self.sub.unsubscribe()
+        await self.js.unsubscribe()
         _f('warn', f'unsubscribed from {self.frequency}')
         await self.nc.drain()
         _f('warn', f'disconnected from {self.server}')
