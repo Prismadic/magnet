58a56e50eee23e2a29e751f5b7cc9e39f030c5ae
DylanAlloy <alloydylan@icloud.com>
2023-12-18 18:28:45 -0500

    🚧 fix(wip): Cleanup

--- magnet/ic/field.py
+++ magnet/ic/field.py
@@ -5,14 +5,15 @@ class Charge:
     def __init__(self, server):
         self.server = server
 
-    async def on(self, frequency: str = 'default'):
+    async def on(self, frequency: str = 'default', stream: str = 'default'):
         self.frequency = frequency
+        self.stream = stream
         try:
             nc = await nats.connect(f'nats://{self.server}:4222')
             self.nc = nc
             self.js = self.nc.jetstream()
-            await self.js.add_stream(name=self.frequency, subjects=[self.frequency])
-            self.sub = await self.js.pull_subscribe(self.frequency, 'workers')
+            await self.js.add_stream(name=self.stream, subjects=[self.frequency])
+            self.sub = await self.js.pull_subscribe(self.frequency, 'magnet-charge')
             _f("success", f'connected to {self.server}')
         except TimeoutError:
             _f('fatal', f'could not connect to {self.server}')
@@ -27,19 +28,20 @@ class Charge:
         except Exception as e:
             print(e)
             _f('fatal', f'could not send data to {self.server}')
-    async def emp(self, name: str = None):
-        await self.js.delete_stream(name=name if name is not None else self.frequency)
+    async def emp(self):
+        await self.js.delete_stream(name=self.stream)
         _f('success', f'{self.frequency} stream deleted')
 
 class Resonator:
 
     def __init__(self, server):
         self.server = server
-    async def on(self, frequency: str = 'default', cb=print):
+    async def on(self, frequency: str = 'default', stream: str = 'default', cb=print):
         self.frequency = frequency
+        self.stream = stream
         self.nc = await nats.connect(f'nats://{self.server}:4222')
         self.js = self.nc.jetstream()
-        self.sub =  await self.js.pull_subscribe(self.frequency, stream=self.frequency)
+        self.sub =  await self.js.pull_subscribe(self.frequency, stream=self.stream)
         while True:
             msgs = await self.sub.fetch(batch=10, timeout=60)
             for msg in msgs:
