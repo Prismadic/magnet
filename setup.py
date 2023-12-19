1bf0cc92a4a250c3c826f065287e715ee32d0622
DylanAlloy <alloydylan@icloud.com>
2023-12-19 06:25:28 -0500

    🧲 feat(release): requirements and tagging

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(
     name='llm_magnet',
-    version='0.0.3',
+    version='0.0.4',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
+        "torch"
+        , "spacy"
+        , "numpy"
+        , "nats-py"
     ],
     url = 'https://github.com/Prismadic/magnet'
 )
