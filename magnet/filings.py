36991a8354abe4a1f785d2e716eef873336a74b5
DylanAlloy <alloydylan@icloud.com>
2023-12-21 11:34:14 -0500

    🚧 fix(workflow): one splitter, more customizable

--- magnet/filings.py
+++ magnet/filings.py
@@ -56,7 +56,7 @@ class Processor:
         if self.df is not None:
             try:
                 _f("wait", f"get coffee or tea - {len(self.df)} processing...")
-                sentence_splitter = self.bge_splitter if splitter is None else splitter
+                sentence_splitter = self.default_splitter if splitter is None else splitter
                 chunks = []
                 knowledge_base = pd.DataFrame()
                 tqdm.pandas()
@@ -85,75 +85,11 @@ class Processor:
                 _f("fatal", e)
         else:
             return _f("fatal", "no data loaded!")
-    def bge_splitter(self, data, window_size=250, overlap=25, nlp=True):
+    def default_splitter(self, data, window_size=768, overlap=76, nlp=True):
         if nlp:
             self.utils.nlp.max_length = len(data) + 100
             sentences = [str(x) for x in self.utils.nlp(data).sents]
-
-            new_sentences = []
-            for sentence in sentences:
-                # Split the sentence into words
-                words = sentence.split()
-
-                # Initialize indices
-                start_idx = 0
-                end_idx = 0
-
-                while end_idx < len(words):
-                    # Calculate the end index based on words, not characters
-                    while end_idx < len(words) and len(' '.join(words[start_idx:end_idx + 1])) <= window_size:
-                        end_idx += 1
-
-                    # Create a chunk with the selected words
-                    chunk = ' '.join(words[start_idx:end_idx])
-
-                    # Check if the chunk is not empty and does not contain individual words longer than window_size
-                    if chunk and all(len(word) <= window_size for word in chunk.split()):
-                        new_sentences.append(chunk)
-
-                    # Update the start index for the next chunk
-                    start_idx = end_idx
-
-            return new_sentences
-        else:
-            # Perform chunked splitting by a fixed character length
-            chunks = []
-            start_char_idx = 0
-            while start_char_idx < len(data):
-                end_char_idx = start_char_idx + window_size
-                chunk = data[start_char_idx:end_char_idx]
-                chunks.append(chunk)
-                start_char_idx += (window_size - overlap)
-            return chunks
-    def mistral_splitter(self, data, window_size=768, overlap=76, nlp=True):
-        if nlp:
-            self.utils.nlp.max_length = len(data) + 100
-            sentences = [str(x) for x in self.utils.nlp(data).sents]
-
-            new_sentences = []
-            for sentence in sentences:
-                tokens = self.utils.nlp.tokenizer(sentence)
-                num_tokens = len(tokens)
-                
-                start_token_idx = 0
-                end_token_idx = 0
-
-                while end_token_idx < num_tokens:
-                    # Calculate the end index based on tokens
-                    while end_token_idx < num_tokens and sum(len(tokens[i].text) for i in range(start_token_idx, end_token_idx + 1)) <= window_size:
-                        end_token_idx += 1
-
-                    # Create a chunk with the selected tokens
-                    chunk = ' '.join(tokens[start_token_idx:end_token_idx])
-
-                    # Check if the chunk is not empty and does not contain individual words longer than window_size
-                    if chunk and all(len(token.text) <= window_size for token in tokens[start_token_idx:end_token_idx]):
-                        new_sentences.append(chunk)
-
-                    # Update the start index for the next chunk
-                    start_token_idx = end_token_idx
-
-            return new_sentences
+            return sentences
         else:
             # Perform chunked splitting by a fixed character length
             chunks = []
