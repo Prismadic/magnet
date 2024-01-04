bb0cb688f89049b0dc6b4197d7c0cb365a5a9112
DylanAlloy <alloydylan@icloud.com>
2024-01-04 08:17:58 -0500

    🧲 feat(release): 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='llm_magnet',
-    version='0.0.9',
+    version='0.1.0',
     description="the small distributed language model toolkit. fine-tune state-of-the-art LLMs anywhere, rapidly.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
