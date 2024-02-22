73de4fd05d06340222ef6647d70f7f84ee0c00b2
DylanAlloy <alloydylan@icloud.com>
2024-02-22 08:41:33 -0500

    🧲 feat(release): 0.2.2

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@ from setuptools import setup
 
 setup(
     name='llm_magnet',
-    version='0.2.1',
+    version='0.2.2',
     description="the small distributed language model toolkit. fine-tune state-of-the-art LLMs anywhere, rapidly.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        "torch"
-        , "spacy"
-        , "numpy"
-        , "nats-py"
-        , "sentence_transformers"
-        , "pymilvus"
-        , "rich"
-        , "xxhash"
-    ],
+        'torch',
+        'sentence-transformers',
+        'pandas',
+        'tqdm',
+        'spacy',
+        'pymilvus',
+        'boto3',
+        'nats-py',
+        'rich',
+        'xxhash'
+    ],,
     url = 'https://github.com/Prismadic/magnet'
 )
