c6363004f4d80cfcc255390c7180746f7eea2379
DylanAlloy <alloydylan@icloud.com>
2024-03-27 17:58:54 -0400

    🚧 fix(wip): sync

--- docker/scripts/roles/index.py
+++ docker/scripts/roles/index.py
@@ -1,36 +1,50 @@
+from magnet.base import Magnet
+from magnet.ic.field import Resonator
+from magnet.ize.memory import Memory
 import os
-from magnet.ize import memory
-from magnet.ic import field
-from magnet.utils.globals import _f
 
 config = {
-    "MILVUS_URI": os.getenv('MILVUS_HOST'),
-    "MILVUS_PORT": int(os.getenv('MILVUS_PORT', 19530)),
-    "MILVUS_USER": os.getenv('MILVUS_USER'),
-    "MILVUS_PASSWORD": os.getenv('MILVUS_PASSWORD'),
-    "DIMENSION": int(os.getenv('DIMENSION', 1024)),
-    "MODEL": os.getenv('MODEL'),
-    "INDEX": os.getenv('INDEX'),
-    "INDEX_PARAMS": {
-        'metric_type': 'COSINE',
-        'index_type':'HNSW',
-        'params': {
-            "efConstruction": int(os.getenv('efConstruction', 40)),
-            "M": int(os.getenv('M', 48))
-        },
+    "host": os.environ.get("HOST"),
+    "credentials": None,
+    "domain": None,
+    "stream_name": os.environ.get("STREAM_NAME"),
+    "category": os.environ.get("CATEGORY"),
+    "kv_name": os.environ.get("KV_NAME"),
+    "session": os.environ.get("SESSION"),
+    "os_name": os.environ.get("OS_NAME"),
+    "index": {
+        "milvus_uri": os.environ.get("MILVUS_URI"),
+        "milvus_port": int(os.environ.get("MILVUS_PORT")),
+        "milvus_user": os.environ.get("MILVUS_USER"),
+        "milvus_password": os.environ.get("MILVUS_PASSWORD"),
+        "dimension": int(os.environ.get("DIMENSION")),
+        "model": os.environ.get("MODEL"),
+        "name": os.environ.get("INDEX_NAME"),
+        "options": {
+            'metric_type': os.environ.get("METRIC_TYPE"),
+            'index_type': os.environ.get("INDEX_TYPE"),
+            'params': {
+                "efConstruction": int(os.environ.get("EF_CONSTRUCTION")),
+                "M": int(os.environ.get("M"))
+            }
+        }
     }
 }
 
-embedder = memory.Embedder(config)
-
-async def handle_payload(payload, msg):
-    await embedder.index(payload, msg, verbose=True)
+magnet = Magnet(config)
+memory = Memory(magnet)
+reso = Resonator(magnet)
 
 async def main():
-    try:
-        reso = field.Resonator(os.getenv('RESONATOR_URI'))
-    except Exception as e:
-        _f('warn', e)
-        main()
-    await reso.on(category=os.getenv('CATEGORY'), session=os.getenv('SESSION'), stream=os.getenv('STREAM'))
-    await reso.listen(cb=handle_payload)
+    await magnet.align()
+    await memory.on()
+    await reso.on()
+
+    async def message_handler(payload, msg):
+        await memory.index(payload, msg, v=1)
+
+    await reso.listen(cb=message_handler)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
